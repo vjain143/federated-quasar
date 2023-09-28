ecbd719bd825c75c5dea67bb054fa7fe46e94653
Vivek Jain <5896522+vjain143@users.noreply.github.com>
2023-09-28 12:24:09 +0100

    Update create-secret.bat

--- docs/create-secret.bat
+++ docs/create-secret.bat
@@ -12,6 +12,33 @@ for /f "tokens=1-6 delims=/:. " %%A in ("%DATE% %TIME%") do (
 REM Set the secret name with the current date and time appended
 set SECRET_NAME=%SECRET_NAME_INPUT%_%TIMESTAMP%
 
+@echo off
+setlocal EnableDelayedExpansion
+
+echo Select an environment:
+echo 1. Development
+echo 2. Testing
+echo 3. Production
+set /p ENV_CHOICE=Enter your choice (1-3): 
+
+if "%ENV_CHOICE%"=="1" (
+    set "ENVIRONMENT=Development"
+) else if "%ENV_CHOICE%"=="2" (
+    set "ENVIRONMENT=Testing"
+) else if "%ENV_CHOICE%"=="3" (
+    set "ENVIRONMENT=Production"
+) else (
+    echo Invalid choice. Exiting.
+    exit /b 1
+)
+
+echo Setting environment to: %ENVIRONMENT%
+set "ENV_VAR_NAME=MY_ENVIRONMENT"
+setx %ENV_VAR_NAME% "%ENVIRONMENT%" /M
+
+echo Environment variable %ENV_VAR_NAME% set to %ENVIRONMENT%.
+
+endlocal
 
 
 kubectl create secret generic %SECRET_NAME% --from-literal=%SECRET_KEY%=%SECRET_VALUE%
