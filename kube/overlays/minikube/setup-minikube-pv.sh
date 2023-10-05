87003e1844083c4d147b054efe3658f888524801
Vivek Jain <5896522+vjain143@users.noreply.github.com>
2023-10-05 23:49:26 +0100

    Fixed codebase to deploy code on local kube cluster

--- kube/overlays/minikube/setup-minikube-pv.sh
+++ kube/overlays/minikube/setup-minikube-pv.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
 # Start Minikube
-#minikube start
+minikube start
 
 # Access Minikube VM and create the directory
 minikube ssh <<EOF
 sudo mkdir -p /data/minio-pv
-exit
-EOF
-
-# Output success message
-echo "The directory /data/minio-pv has been created."
-
-minikube ssh <<EOF
 sudo mkdir -p /data/mysql-pv
 exit
 EOF
 
 # Output success message
-echo "The directory /data/mysql-pv has been created."
+echo "The directory /data/minio-pv and /data/mysql-pv has been created."
 
 echo "Minikube setup completed."
