a8e082e9fedbb70607189f836397e30990e110e9
Vivek Jain <5896522+vjain143@users.noreply.github.com>
2023-10-12 00:08:13 +0100

    Added Cloudbeaver support

--- docker/open-metadata/README.md
+++
@@ -1,4 +0,0 @@
-Build Docker Images
-```text
-docker build --tag open-metadata:1.0.1 --no-cache .
-```
--- kube/overlays/minikube/setup-mysql-db.sh
+++ kube/overlays/minikube/setup-mysql-db.sh
@@ -1,12 +1,18 @@
 kubectl exec -it <mysql-pod-name> -n <namespace> -- bash
 
 # Connect to MySQL server using MySQL CLI
-mysql -u <username> -p
+mysql -u root -p
 
 # Replace <username> with your MySQL username and enter the password when prompted
 
 # Now, you're in the MySQL CLI. Create a database (replace <database_name> with your desired database name)
-CREATE DATABASE <database_name>;
+CREATE DATABASE metastore_db;
 
+CREATE DATABASE openmetadata_db;
+CREATE USER 'openmetadata_user'@'%' IDENTIFIED BY 'openmetadata_password';
+GRANT ALL PRIVILEGES ON openmetadata_db.* TO 'openmetadata_user'@'%' WITH GRANT OPTION;
+commit;
 # Exit MySQL CLI
 exit
+
+
